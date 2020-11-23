10a8bd290700eba50bcb7a6f0b5db65757e15e41
Aniket762 <aniketindian8@gmail.com>
2020-11-23 04:42:02 -0500

    v0.1

--- app2.py
+++ app2.py
@@ -3,14 +3,14 @@ import tweepy
 import time
 
 auth = tweepy.OAuthHandler('WMfVJfOA6eBlaoLPkOTfDXdhL','a63Dqba8R7lfDsQtkrcwTPIV90qLVHQgsmaMnod16uFeeOcGcD')
-auth.set_access_token('1288198847101980672-v5pLMnapQbwsduoD6SnteuQhh1eWws','fmC3txEy1ShxAozphVx7UPZiwydFh4UJ4Ukhckid7dJWM')
+auth.set_access_token('1288198847101980672-SCmkp3KdhF8hf9aLSwnxoL6VQ4Adeu','V6hZ9td82HUdVlZwJzaiwwFe7oPLQkfPSvu8vGwZGJ1wV')
 api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
 
 user = api.me()
 print(user)
 
-search = ''
+search = 'Hackathons'
 nrTweets = 500
 
 # for liking tweets
@@ -25,10 +25,10 @@ for tweet in tweepy.Cursor(api.search,search).items(nrTweets):
     except StopIteration:
         break
 
-
+'''
     this is for liking the tweets
 
-'''
+
 # for retweeting
 for tweet in tweepy.Cursor(api.search,search).items(nrTweets):
     try:
