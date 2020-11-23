8af5f6eab58c6abb5572f954366f8523a7d61ae8
Aniket762 <aniketindian8@gmail.com>
2020-11-23 02:13:16 -0500

    hastag search

--- app.py
+++ app.py
@@ -12,6 +12,23 @@ class Twitterbot:
         bot = self.bot
         bot.get('https://twitter.com')
         time.sleep(10)
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+
+    def like_tweet(self,hastag):
+        bot = self.bot
+        bot.get('https://twitter.com/search?q='+hastag+'&src=typd')
+        time.sleep(3)
+
 
 
 ed = Twitterbot('username','password')
+ed.login()
+ed.like_tweet('hackathon')
