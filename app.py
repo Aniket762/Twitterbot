7968baba1e8d7a0541b51b359a02476aba4cd343
Aniket762 <aniketindian8@gmail.com>
2020-11-23 02:26:28 -0500

    Like added

--- app.py
+++ app.py
@@ -26,6 +26,22 @@ class Twitterbot:
         bot = self.bot
         bot.get('https://twitter.com/search?q='+hastag+'&src=typd')
         time.sleep(3)
+        for i in range(1,3):
+            bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            time.sleep(2)
+            
+            tweets = bot.find_elements_by_class_name('tweet')
+            
+            links = [elem.get_attribute('data-permalink-path') for elem in tweets]
+            for link in links:
+                bot.get('https://twitter.com' + link)
+                try:
+                    bot.find_elements_by_class_name('HeartAnimation').click()
+                    time.sleep(50)
+                except Exception as ex:
+                    time.sleep(60)
+
+            #scrolls down used js
 
 
 
